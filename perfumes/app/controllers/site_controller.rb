20e3747ea468b6237b70bb8f1159f7731ed7cdf1
swathi vemuri <vemuri453@gmail.com>
2012-12-10 00:21:43 +0000

    trying to use apis but error occured
    adding outlook

---
+++ perfumes/app/controllers/site_controller.rb
@@ -0,0 +1,10 @@
+class SiteController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def brand
+  end
+end
---
+++ perfumes/test/functional/site_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class SiteControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get brand" do
+    get :brand
+    assert_response :success
+  end
+
+end
